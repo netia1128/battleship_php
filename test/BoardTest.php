df86409d3e32192773bdb13c9564391024f37424
Netia <netia1128@hotmail.com>
2021-06-27 19:58:10 -0600

    Stand up evaluator skeleton

--- test/BoardTest.php
+++ test/BoardTest.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once 'board.php';
+require_once 'ship.php';
 use PHPUnit\Framework\TestCase;
 
 
@@ -11,6 +12,21 @@ class BoardTest extends TestCase
     $board = new Board(4);
     
     $this->assertTrue($board->board_dimension == 4);
+  }
+
+  public function testMakeBoardArray() {
+    $board = new Board(4);
+    $board_array = $board->make_board_array();
+
+    $this->assertTrue($board_array == null);
+  }
+
+  public function testIsValidPlacement() {
+    $board = new Board(4);
+    $coordinates = array("A1", "A2");
+    $ship = new Ship("Submarine", 2);
+
+    $this->assertTrue($board->is_valid_placement($coordinates, $ship));
   }
 }
 
--- test/board.php
+++ test/board.php
@@ -1,34 +1,63 @@
 <!-- require_relative 'evaluator'
 require_relative 'cell' #added -->
 
+
 <?php
+require_once 'evaluator.php';
 
 class Board {
 
   public function __construct($board_dimension) {
     $this->board_dimension = $board_dimension;
     // $this->cells = $this->make_board_hash;
+    $this->evaluator = new Evaluator(array("A1", "A2"));
   }
-  //   @board_dimension = board_dimension
   //   @cells = make_board_hash
   //   @evaluator = Evaluator.new(@cells)
   // end
 
-  // def make_board_hash
-  //   board_array = make_board_array
-  //   board_hash = {}
-  //   board_array.each do |coordinate|
-  //     board_hash[coordinate.to_sym] = Cell.new(coordinate)
-  //   end
-  //   board_hash
-  // end
+
+
+  // public function make_board_hash() {
+  //   $board_array = $this->make_board_array;
+  //   $board_hash = array();
+  //   foreach($board_array as $coordinate) {
+  //     // find out how to add new KV pair to associative array
+  //     // find out how to convert a string to a symbol, if even possible
+  //     $board_hash($coordinate=>new Cell(coordinate);
+  //       //   board_array.each do |coordinate|
+  //       //     board_hash[coordinate.to_sym] = Cell.new(coordinate)
+  //       //   end
+  //   }
+  //   return $board_hash
+  // }
+
+  public function make_board_array() {
+    $board_array = array();
+    $letters = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    // how to create a range as shown below
+    // $letters = ("A" .. "Z")->__to_array();
+    //   letters = ("A" .. "Z").to_a
+    $letter_count = 0;
+    $number_count = 1;
+    echo $this->board_dimension;
+    $total_coordinates = $this->board_dimension ** 2;
+
+    for($letter_count = 0; $letter_count <= $this->board_dimension; $letter_count++) {
+      for($num_count = 1; $num_count <= $this->board_dimension; $num_count++) {
+        // How to add to an array as shown below
+        // $board_array << $letters[$letter_count] + $num_count;
+      }
+    }
+
+    return $board_array;
+  }
 
   // def make_board_array
   //   board_array = []
-  //   letters = ("A" .. "Z").to_a
   //   letter_count = 0
   //   number_count = 1
-  //   # require 'pry'; binding.pry
+  //   # require 'pry'; binding.prys
   //   total_coordinates = @board_dimension * @board_dimension
 
   //   @board_dimension.times do
@@ -42,6 +71,10 @@ class Board {
   //   board_array
   // end
 
+  public function is_valid_placement($coordinates, $ship) {
+    $valid_placement = $this->evaluator->coordinates_match_ship_length($coordinates, $ship);
+    return $valid_placement;
+  }
   // def valid_placement?(coordinates, ship)
   //   @evaluator.coordinates_match_ship_length?(coordinates, ship) &&
   //   @evaluator.coordinates_empty?(coordinates, @cells) &&
