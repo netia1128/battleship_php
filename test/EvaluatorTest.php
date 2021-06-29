4798c307e94dadaca942072e0d6ac9b1b40cf4a1
Netia <netia1128@hotmail.com>
2021-06-29 14:22:04 -0600

    Add test for evaluator #coordinates_match_ship_length

---
+++ test/EvaluatorTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+require_once 'evaluator.php';
+require_once 'board.php';
+require_once 'ship.php';
+
+use PHPUnit\Framework\TestCase;
+
+
+class EvaluatorTest extends TestCase
+{
+
+  public function testEvaluatorInitialization() {
+    $board = new Board(2);
+    $evaluator = new Evaluator($board->cells);
+    $evaluator_cell1 = $evaluator->cells['A1'];
+    
+    $this->assertTrue(count($evaluator->cells) == 4);
+    $this->assertTrue(is_a($evaluator_cell1, 'Cell'));
+    
+  }
+
+  public function testCoordinatesMatchShipLength() {
+    $board = new Board(2);
+    $evaluator = new Evaluator($board->cells);
+    $ship = new Ship("Submarine", 2);
+    $coordinates = ['A1', 'A2'];
+
+    $this->assertTrue($evaluator->coordinates_match_ship_length($coordinates, $ship));
+
+    $ship = new Ship("Destroyer", 4);
+    $coordinates = ['A1', 'A2'];
+
+    $this->assertTrue($evaluator->coordinates_match_ship_length($coordinates, $ship));
+  }
+}
+
+// phpunit ShipTest
