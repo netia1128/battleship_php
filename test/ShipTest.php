84870d3aedcc991fe4902ddf65e5a4bd027bafa7
Netia <netia1128@hotmail.com>
2021-06-30 14:56:47 -0600

    Correct file paths

---
+++ test/ShipTest.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once 'ship.php';
+use PHPUnit\Framework\TestCase;
+
+
+class ShipTest extends TestCase
+{
+
+  public function setUp(): void {
+      $sub = new Ship("Submarine", 1);
+  }
+
+  public function testShipInitialization() {
+    $ship = new Ship("Destroyer", 4);
+    $name = $ship->name;
+    $length = $ship->length;
+    $health = $ship->health;
+    $expected_name = "Destroyer";
+    $expected_length = 4;
+    $expected_health = 4;
+    $this->assertTrue($name == $expected_name);
+    $this->assertTrue($length == $expected_length);
+    $this->assertTrue($health == $expected_health);
+  }
+
+  public function testShipHit() {
+    $ship = new Ship("Destroyer", 4);
+    $ship->hit();
+    $health = $ship->health;
+    $expected_health = 3;
+    $this->assertTrue($health == $expected_health);
+  }
+
+  public function testShipSunk() {
+    $ship = new Ship("Destroyer", 4);
+
+    $this->assertFalse($ship->is_sunk());
+
+    $ship->hit();
+    $ship->hit();
+    $ship->hit();
+    $ship->hit();
+
+    $this->assertTrue($ship->is_sunk());
+  }
+}
+
+// phpunit ShipTest 
