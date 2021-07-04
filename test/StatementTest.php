6e805f057ad436ad36312cc35d3c39c6563b88cb
Netia <netia1128@hotmail.com>
2021-07-03 21:03:02 -0600

    Add progress on statement class transfer

--- test/StatementTest.php
+++ test/StatementTest.php
@@ -55,19 +55,13 @@ class StatementTest extends TestCase
       "Sorry Netia, that is not a valid board size.\n" .
       "Please choose a board size between 4 and 9 cells wide. \n");
   }
-  // public function test_computron_won() 
-  // {
-  //   $statement = new Statement;
-    
-  //   $this->assertTrue($statement->battleship_graphic() ===
 
-  // }
-  // describe '#computron_won' do
-  //   it 'contains the board_dimension_error statement' do
-  //     expect(@statement.computron_won).to eq(  "Computron sunk all of your ships! \n" .
-  //       "Computron won!")
-  //   end
-  // end
+  public function test_computron_won() 
+  {
+    $this->assertTrue($this->statement->computron_won() === "Computron sunk all of your ships! \n:" .
+    "Computron won!");
+  }
+
   public function test_difficulty_level_error()
   {
     $this->assertTrue($this->statement->difficulty_level_error() === "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \n" .
@@ -77,11 +71,12 @@ class StatementTest extends TestCase
            " \n" .
            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \n");
   }
-  // describe '#game_over' do
-  //   it 'contains the board_dimension_error statement' do
-  //     expect(@statement.game_over).to eq("GAMEOVER!")
-  //   end
-  // end
+
+  public function test_game_over()
+  {
+    $this->assertTrue($this->statement->game_over() === "GAMEOVER!");
+  }
+
   public function test_introduction() 
   {
     $this->assertTrue($this->statement->introduction() === "Hi Netia. \n" .
@@ -114,7 +109,7 @@ class StatementTest extends TestCase
 
     $this->assertTrue($this->statement->ship_placement_error($player, $ship) === "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \n" .
         " \n" .
-        "Sorry #{@name}, your placement is not valid.\n" .
+        "Sorry Netia, your placement is not valid.\n" .
         "For a valid placement each of the following must be true:\n" .
         "- Please provide a number of coordinates equal to the ship length\n" .
         "- The coordinates must be consecuitive\n" .
@@ -133,10 +128,10 @@ class StatementTest extends TestCase
         "Please provide #{ship.length} coordinate(s): \n");
   }
 
-  public function ship_placement_explanation()
+  public function test_ship_placement_explanation()
   {
     $player = new Player(4);
-    $this->assertTrue($this->statement->hip_placement_explanation($player) === "Great! Now let's place your ships.\n" .
+    $this->assertTrue($this->statement->ship_placement_explanation($player) === "Great! Now let's place your ships.\n" .
       " \n" .
       "We each have three ships.\n" .
       "    -The Cruiser, which is three cells long.\n" .
@@ -147,7 +142,7 @@ class StatementTest extends TestCase
       " \n" .
       "Let's start. Here is your board: \n" .
       " \n" .
-      $player.board.render(true) .
+      $player->board->render(true) .
       " \n" .
       "You will choose cells to put the ships in.\n" .
       "Please provide the coordinate of each cell" .
@@ -156,95 +151,88 @@ class StatementTest extends TestCase
       "   A1 A2 A3\n" .
       " \n");
   }
-  // describe '#ship_placement_success' do
-  //   it 'contains the ship placement success statement' do
-  //     ship = Ship.new("Tug Boat", 1)
-  //     expect(@statement.ship_placement_success(ship, @player)).to eq("Great job #{@name}, you've placed your #{ship.name}!\n" .
-  //     "Here is what your board looks like now.\n" .
-  //     "S means there is a ship in a cell. \n" .
-  //     " \n" .
-  //     @player.board.render(true) .
-  //     " \n")
-  //   end
-  // end
-  // describe '#ship_placement_success' do
-  //   it 'contains the ship placement success statement' do
-  //     ship = Ship.new("Tug Boat", 1)
-  //     expect(@statement.ship_placement_success(ship, @player)).to eq(    "Great job #{@name}, you've placed your #{ship.name}!\n" .
-  //         "Here is what your board looks like now.\n" .
-  //         "S means there is a ship in a cell. \n" .
-  //         " \n" .
-  //         @player.board.render(true) .
-  //         " \n")
-  //   end
-  // end
-  // describe '#take_turn' do
-  //   it 'contains the take turn statement' do
-  //     player = Player.new(4)
-  //     computron = Player.new(4)
-  //     expect(@statement.take_turn(player, computron)).to eq(    " \n" .
-  //         "=============COMPUTRON BOARD============= \n" .
-  //         " \n" .
-  //         computron.board.render .
-  //         " \n" .
-  //         "==============PLAYER BOARD============== \n" .
-  //         " \n" .
-  //         player.board.render(true) .
-  //         " \n" .
-  //         "Please pick a coordinate on Computron's board to fire upon:\n")
-  //   end
-  //   describe '#take_turn_error' do
-  //     it 'contains the take turn error statement' do
-  //       player = Player.new(4)
-  //       computron = Player.new(4)
-  //       expect(@statement.take_turn_error(player, computron)).to eq(        "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" .
-  //           " \n" .
-  //           "\nSorry #{@name} " .
-  //           "Your shot coordinate is not valid.\n" .
-  //           "To have a valid shot placement all of the following must be true:\n" .
-  //           "- The coordinate must be on the board.\n" .
-  //           "- You cannot already have fired upon the coordinate.\n" .
-  //           "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" .
-  //           "\n" .
-  //           '=============COMPUTRON BOARD=============' .
-  //           " \n" .
-  //           computron.board.render .
-  //           " \n" .
-  //           '==============PLAYER BOARD==============' .
-  //           " \n" .
-  //           player.board.render(true) .
-  //           " \n" .
-  //           "Please try again.\n")
-  //     end
-  //   end
-  // end
-  // describe '#turn_explanation' do
-  //   it 'contains the turn explanation statement' do
-  //     expect(@statement.turn_explanation).to eq("Great work, all your ships have been placed. \n" .
-  //       "Let me quickly explain how to play. \n" .
-  //       " \n" .
-  //       "To play you will choose a cell on my board to fire upon.\n" .
-  //       "To do this, provide the coordinate of the cell you wish to fire upon.\n" .
-  //       "For example: A1\n" .
-  //       "When you are done, I will fire upon your board.\n" .
-  //       " \n" .
-  //       "After we each take our turn, I will summarize what happened and update " .
-  //       "the board as follows: \n" .
-  //       "  - . represents a cell that has not been fired on yet\n" .
-  //       "  - S represents your ships (we cannot see each others ships)\n" .
-  //       "  - M represents a miss\n" .
-  //       "  - H represents a hit\n" .
-  //       "  - X represents a sunk ship \n" .
-  //       " \n" .
-  //       "We will take turns until all of someone's ships have been sunk.\n" .
-  //       " \n" .
-  //       "Now let's play")
-  //     end
-  //   end
-  //   describe '#you_won' do
-  //     it 'contains the you won statement' do
-  //       expect(@statement.you_won).to eq("You sunk all of Computron's ships! \n" .
-  //       "You won!")
-  //     end
-  //   end
+  public function test_ship_placement_success()
+  {
+      $ship = new Ship("Tug Boat", 1);
+      $player = new Player(4);
+    
+      $this->assertTrue($this->statement->ship_placement_success($ship, $player) === "Great job Netia, you've placed your $ship->name!\n" .
+      "Here is what your board looks like now.\n" .
+      "S means there is a ship in a cell. \n" .
+      " \n" .
+      $player->board->render(true) .
+      " \n");
+  }
+
+  public function test_take_turn()
+  {
+    $player = new Player(4);
+    $computron = new Player(4);
+
+    $this->assertTrue($this->statement->take_turn($player, $computron) === " \n" .
+        "=============COMPUTRON BOARD============= \n" .
+        " \n" .
+        $computron->board->render() .
+        " \n" .
+        "==============PLAYER BOARD============== \n" .
+        " \n" .
+        $player->board->render(true) .
+        " \n" .
+        "Please pick a coordinate on Computron's board to fire upon:\n");
+  }
+
+  public function test_take_turn_error()
+  {
+    $player = new Player(4);
+    $computron = new Player(4);
+
+    $this->assertTrue($this->statement->take_turn_error($player, $computron) === 
+        "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" .
+        " \n" .
+        "\nSorry Netia " .
+        "Your shot coordinate is not valid.\n" .
+        "To have a valid shot placement all of the following must be true:\n" .
+        "- The coordinate must be on the board.\n" .
+        "- You cannot already have fired upon the coordinate.\n" .
+        "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" .
+        "\n" .
+        '=============COMPUTRON BOARD=============' .
+        " \n" .
+        $computron->board->render() .
+        " \n" .
+        '==============PLAYER BOARD==============' .
+        " \n" .
+        $player->board->render(true) .
+        " \n" .
+        "Please try again.\n");
+  }
+
+  public function test_turn_explanation()
+  {
+    $this->assertTrue($this->statement->turn_explanation() === "Great work, all your ships have been placed. \n" .
+        "Let me quickly explain how to play. \n" .
+        " \n" .
+        "To play you will choose a cell on my board to fire upon.\n" .
+        "To do this, provide the coordinate of the cell you wish to fire upon.\n" .
+        "For example: A1\n" .
+        "When you are done, I will fire upon your board.\n" .
+        " \n" .
+        "After we each take our turn, I will summarize what happened and update " .
+        "the board as follows: \n" .
+        "  - . represents a cell that has not been fired on yet\n" .
+        "  - S represents your ships (we cannot see each others ships)\n" .
+        "  - M represents a miss\n" .
+        "  - H represents a hit\n" .
+        "  - X represents a sunk ship \n" .
+        " \n" .
+        "We will take turns until all of someone's ships have been sunk.\n" .
+        " \n" .
+        "Now let's play \n");
+  }
+
+  public function you_won()
+  {
+    $this->assertTrue($this->statement->you_won() === "You sunk all of Computron's ships! \n" .
+      "You won!");
+  }
 }
